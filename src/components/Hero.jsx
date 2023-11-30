83673766012f8387e1f5eb267bbaa5269223c98f
Devansh Pehlajani <devanshrockz11@gmail.com>
2023-11-30 16:01:01 +0530

    esummit4

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,9 @@
 // Hero.js
 import React from 'react';
-import StickyComponents from './StickyComponents';
 
 const Hero = () => {
   return (
-    <div className="hero min-h-screen relative mt-[5rem]" style={{ backgroundImage: 'url(images/esum1.jpg)' }}>
+    <div className="hero min-h-screen relative mt-[5rem] " style={{ backgroundImage: 'url(images/esum1.jpg)' }}>
       <div className="hero-overlay bg-opacity-0"></div>
       <div className="flex-wrap flex absolute bottom-[5vh] right-[1vw] w-[20rem] md:w-[32rem] items-center ">
         {/* Your hero content goes here */}
--- src/pages/Esummit.js
+++ src/pages/Esummit.js
@@ -5,11 +5,12 @@ import StickyComponents from '../components/StickyComponents';
 
 const Esummit = () => {
   const [imageSource, setImageSource] = useState('images/fffbg3.png'); // Initial image source
-
+  const [imageSource1, setImageSource1] = useState('images/esum1.jpg');
   useEffect(() => {
     const handleResize = () => {
       const screenWidth = window.innerWidth;
       setImageSource(screenWidth > 768 ? 'images/fffbg3.png' : 'images/fffbg4.png');
+      setImageSource1(screenWidth > 768 ? 'images/esum1.jpg' : 'images/fffbg5.png');
     };
 
     handleResize();
@@ -27,7 +28,11 @@ const Esummit = () => {
     <div className="relative">
       <Navbar />
       <div className='flex flex-col items-center justify-center'>
-        <Hero />
+      <img
+          src={imageSource1}
+          alt='Zoomed Out Image'
+          className="object-cover object-center w-screen  h-auto md:scale-90 md:mt-0 mt-[4rem] mb-[2rem] "
+        />
         <img
           src={imageSource}
           alt='Zoomed Out Image'
